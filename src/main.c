160325e932539546213b6e8945293390c8b4775a
mg643l <55359157+mg643l@users.noreply.github.com>
2026-05-07 23:18:35 +0100

    LED Blink Test

--- src/main.c
+++ src/main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_PIN 26
 
 void app_main() {
+    gpio_reset_pin(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    // ReSharper disable once CppDFAEndlessLoop
     while (1) {
-        printf("Hello world!\n");
+        printf("LED ON\n");
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        printf("LED OFF\n");
+        gpio_set_level(LED_PIN, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
